feb2cc4e35527a17f1b5fb98913e88ff922821f2
daniel <danielchungara1@gmail.com>
2021-05-18 14:23:53 -0300

    Implementing dashboard structure. #3

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './view/components/dashboard/dashboard.component';
+import { HeaderComponent } from './view/components/header/header.component';
+
+
+@NgModule({
+  declarations: [DashboardComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
