43b100ad8d143a42f1a7eda757f93a2e19078ce9
daniel <danielchungara1@gmail.com>
2021-05-23 04:40:01 -0300

    Header styles. #10

--- src/app/dashboard/access/shared/header/header.component.ts
+++ src/app/dashboard/access/shared/header/header.component.ts
@@ -3,6 +3,7 @@ import {LocalStorageService} from '@core/localStorage/local-storage.service';
 import {UserModel} from '../../models/UserModel';
 import {Router} from '@angular/router';
 import {NotificationService} from '@shared/notifications/notification.service';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +14,8 @@ export class HeaderComponent implements OnInit {
 
   user: UserModel;
 
+  items: MenuItem[];
+
   constructor(private localStorageService: LocalStorageService,
               private router: Router,
               private notificationService: NotificationService) {
@@ -20,9 +23,22 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.localStorageService.getUser();
+
+    this.items = [
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        routerLink: ['edit-user', this.user.id]
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-fw pi-sign-out',
+        command: () => this.logout()
+      }
+    ];
   }
 
-  logout(): void{
+  logout(): void {
     this.localStorageService.clear();
     this.router.navigate(['security/login']);
     this.notificationService.showSuccess('User logged out.');
