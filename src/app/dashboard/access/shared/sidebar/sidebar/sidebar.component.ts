173b99c10fbbd259ac8e62d6bee1ebac31c21607
daniel <danielchungara1@gmail.com>
2021-05-23 17:04:28 -0300

    Sidebar styles. #11

--- src/app/dashboard/access/shared/sidebar/sidebar/sidebar.component.ts
+++ src/app/dashboard/access/shared/sidebar/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  items: MenuItem[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Tplate Software',
+        icon: 'fas fa-home',
+        routerLink: ['users']
+      },
+      {
+        label: 'List Users',
+        icon: 'fas fa-users',
+        routerLink: ['users']
+      },
+      {
+        label: 'Create User',
+        icon: 'fas fa-user-plus',
+        routerLink: ['create-user']
+      }
+    ];
   }
 
 }
