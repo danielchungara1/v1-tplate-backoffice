0a37e1842f95fbddbec11f2fbf545861611fe68d
daniel <danielchungara1@gmail.com>
2021-05-30 01:59:33 -0300

    Changing form styles. #28

--- src/app/dashboard/access/components/brand/brand-list/brand-list.component.ts
+++ src/app/dashboard/access/components/brand/brand-list/brand-list.component.ts
@@ -6,7 +6,7 @@ import {BrandListService} from '../../../../business/services/brand/brand-list.s
 @Component({
   selector: 'app-brand-list',
   templateUrl: './brand-list.component.html',
-  styleUrls: ['./brand-list.component.css']
+  styleUrls: ['./brand-list.component.scss']
 })
 export class BrandListComponent implements OnInit {
 
